33308c08b59c7a3fba2a37b43af0a788c9fd127c
Dechichi01 <gabrieldechichi@gmail.com>
2016-07-19 15:45:22 -0300

    added 2 options of mobile input

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 [RequireComponent (typeof (PlayerController))]
 [RequireComponent (typeof (GunController))]
@@ -8,35 +9,67 @@ public class Player : LivingEntity {
 	public float moveSpeed = 5f;
 	PlayerController controller;
 	GunController gunController;
-
 	Camera viewCamera;
+    public Joystick rotateJoyStick;
+    public RectTransform aimJoystickRect;
+    private SwipeControls swipeControl;
+    Vector3 aimVelocity;
 
 	override protected void Start () {
 		base.Start();
 		controller = GetComponent<PlayerController>();
 		gunController = GetComponent<GunController>();
+		swipeControl = GetComponent<SwipeControls>();
 		viewCamera = Camera.main;
 	}
 
 	void Update () {
+
 		GetInputAndMove ();
+        //GetInputAndRotate();
+        GetSwipeAndAim();
 
-		GetInputAndLook (); 
+		//GetInputAndLook (); 
 
-		if (Input.GetMouseButton(0)){
-			gunController.Shoot();
-		}
+		//if (Input.GetMouseButton(0)){
+		//	gunController.Shoot();
+		//}
 
 	}
 
 	void GetInputAndMove ()
 	{
-		Vector3 moveInput = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+		Vector3 moveInput = new Vector3 (CrossPlatformInputManager.GetAxisRaw ("HorzLeft"), 0, CrossPlatformInputManager.GetAxisRaw ("VertLeft"));
 		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
 		controller.Move (moveVelocity);
 	}
 
-	void GetInputAndLook ()
+    void GetInputAndRotate()
+    {
+    	if (CrossPlatformInputManager.GetAxisRaw("HorzRight") != 0 || CrossPlatformInputManager.GetAxisRaw("VertRight") !=0)
+    	{
+			Vector3 vec = (rotateJoyStick.transform.position - rotateJoyStick.m_StartPos).normalized;
+        	vec = new Vector3(vec.x,0,vec.y);
+        	Vector3 point = Vector3.zero + vec*10f;
+        	controller.LookAtPoint(point);
+    	}
+    }
+
+    void GetSwipeAndAim()
+    {
+    	Vector2 point;
+    	bool boolean = RectTransformUtility.RectangleContainsScreenPoint(aimJoystickRect, Input.mousePosition);
+    	switch(swipeControl.GetSwipeDirection()){
+    	case SwipeControls.SwipeDirection.Right:
+			transform.Rotate(new Vector3(0f,90f,0f));
+			break;
+    	case SwipeControls.SwipeDirection.Left:
+			transform.Rotate(new Vector3(0f,-90f,0f));
+			break;
+    	}
+    }
+
+    void GetInputAndLook ()
 	{
 		Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
 		//return a ray that start in the camera and goes into the given position
---
+++ Assets/Scripts/SwipeControls.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeControls : MonoBehaviour {
+
+    public enum SwipeDirection
+    {
+        Null = 0, //no swipe detected
+        Duck = 1, //swipe down detected
+        Jump = 2, //swipe up detected
+        Right = 3, //swipe right detected
+        Left = 4 //swipe left detected
+    }
+
+    private float fSensitivity = 15f;
+    private float fInitialX;
+    private float fInitialY;
+    private float fFinalX;
+    private float fFinalY;
+
+    private float inputX; //x-coordinate
+    private float inputY; //y-coordinate
+    private float slope; //slope (m) of the
+    private float fDistance; //magnitude of dstance between two positions
+    private int iTouchStateFlag; //flag to check
+    private SwipeDirection sSwipeDirection; //receives swipe output
+
+	// Use this for initialization
+	void Start () {
+        fInitialX = fInitialY = fFinalX = fFinalY = 0f;
+        inputX = inputY = 0f;
+
+        iTouchStateFlag = 0;
+        sSwipeDirection = SwipeDirection.Null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (iTouchStateFlag == 0 && Input.GetMouseButtonDown(0))    //state 1 of swipe control
+        {
+            fInitialX = Input.mousePosition.x;  //get the initial x mouse/ finger value
+            fInitialY = Input.mousePosition.y;  //get the initial y mouse/ finger value
+
+            sSwipeDirection = SwipeDirection.Null;
+            iTouchStateFlag = 1;
+        }
+        if (iTouchStateFlag == 1)   //state 2 of swipe control
+        {
+            fFinalX = Input.mousePosition.x;
+            fFinalY = Input.mousePosition.y;
+
+            sSwipeDirection = GetInputSwipeDirection(); //get the swipe direction
+            if (sSwipeDirection != SwipeDirection.Null)
+                iTouchStateFlag = 2;
+        }//end of state 1		
+        if (iTouchStateFlag == 2 || Input.GetMouseButtonUp(0))  //state 3 of swipe control
+        {
+            iTouchStateFlag = 0;
+        }//end of M.R. swipe control
+    }
+
+    public SwipeDirection GetSwipeDirection() // to be used by Update()
+    {
+        if (sSwipeDirection != SwipeDirection.Null)//if a swipe is detected
+        {
+            SwipeDirection etempSwipeDirection = sSwipeDirection;
+            sSwipeDirection = SwipeDirection.Null;
+
+            return etempSwipeDirection;
+        }
+        else
+        {
+            return SwipeDirection.Null;//if no swipe was detected
+        }
+    }
+
+    private SwipeDirection GetInputSwipeDirection()
+    {
+        //calculate the slope of the swipe
+        inputX = fFinalX - fInitialX;
+        inputY = fFinalY - fInitialY;
+        slope = inputY / inputX;
+
+        //calculate the distance of tap start and end
+        fDistance = Mathf.Sqrt(Mathf.Pow((fFinalY - fInitialY), 2) + Mathf.Pow((fFinalX - fInitialX), 2));
+
+        if (fDistance <= (Screen.width / fSensitivity))//higher the dividing factor higher the sensitivity
+            return SwipeDirection.Null;
+
+        if (inputX >= 0 && inputY > 0 && slope > 1)//first octant JUMP
+        {
+            return SwipeDirection.Jump;
+        }
+        else if (inputX <= 0 && inputY > 0 && slope < -1)//eighth octant  JUMP
+        {
+            return SwipeDirection.Jump;
+        }
+        else if (inputX > 0 && inputY >= 0 && slope < 1 && slope >= 0)//second octant  RIGHT
+        {
+            return SwipeDirection.Right;
+        }
+        else if (inputX > 0 && inputY <= 0 && slope > -1 && slope <= 0)//third octant  RIGHT
+        {
+            return SwipeDirection.Right;
+        }
+        else if (inputX < 0 && inputY >= 0 && slope > -1 && slope <= 0)//seventh octant  LEFT
+        {
+            return SwipeDirection.Left;
+        }
+        else if (inputX < 0 && inputY <= 0 && slope >= 0 && slope < 1)//sixth octant  LEFT
+        {
+            return SwipeDirection.Left;
+        }
+        else if (inputX >= 0 && inputY < 0 && slope < -1)//fourth octant  DUCK
+        {
+            return SwipeDirection.Duck;
+        }
+        else if (inputX <= 0 && inputY < 0 && slope > 1)//fifth octant  DUCK
+        {
+            return SwipeDirection.Duck;
+        }//end of else if
+
+        return SwipeDirection.Null;
+    }
+}
