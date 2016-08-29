6853a99aa1fa28b40ac2b352068cfcb963f67dde
Dechichi01 <gabrieldechichi@gmail.com>
2016-08-29 18:19:39 -0300

    Made level prototype

---
+++ Assets/Scripts/BreakableObstacle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableObstacle : LivingEntity {
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BoxCollider))]
+[RequireComponent (typeof (NavMeshObstacle))]
+public class Obstacle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
